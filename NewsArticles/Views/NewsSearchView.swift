f32d8bfa3f46ad9f78429b1496b4535dc375b2e6
Anil Punjabi <anilo@hotmail.com>
2021-04-20 00:36:15 -0700

    view in progress

---
+++ NewsArticles/Views/NewsSearchView.swift
@@ -0,0 +1,17 @@
+//
+//  NewsSearchView.swift
+//  NewsArticles
+//
+//  Created by Renu Punjabi on 4/15/21.
+//
+
+import Foundation
+import UIKit
+
+final class NewsSearchView: UITableView {
+    
+    private lazy var newsSearchTableView: UITableView = {
+        return UITableView(frame: .zero)
+    }()
+    
+}

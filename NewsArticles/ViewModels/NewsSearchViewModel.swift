bd04c568c7ce9e6fbc66d6d831d6e39b5b589b2d
Renu Punjabi <rpunjabi12@gmail.com>
2021-04-21 00:17:00 -0700

    solved the crash

--- NewsArticles/ViewModels/NewsSearchViewModel.swift
+++ NewsArticles/ViewModels/NewsSearchViewModel.swift
@@ -26,22 +26,19 @@ final class NewsSearchViewModel {
             switch result {
             case .success(let news):
                 let response = news.response
-                var imagesURLArray = [String]()
+                var imagesDictionary = [String : String]()
                 for ele in response.docs {
                     let multiArr = ele.multimedia
                     
                     for ele in multiArr {
                         if ele.subtype == "thumbnail" {
-                            imagesURLArray.insert(ele.url, at: 0)
+                            imagesDictionary["thumb"] = ele.url
                         } else if ele.subtype == "watch268" {
-                            imagesURLArray.append(ele.url)
+                            imagesDictionary["watch"] = ele.url
                         }
                     }
-                    let thumbURL = (imagesURLArray.first != nil) ? imagesURLArray[0] : ""
-                    let detailURL = (imagesURLArray.last != nil) ? imagesURLArray[1] : ""
-                    print("thumb = \(thumbURL)")
-                    print("detail = \(detailURL)")
-                    let details = NewsSearchDetails(thumbnailURL: thumbURL, detailImageURL: detailURL, headline: ele.headline.main, abstract: ele.abstract, webURL: ele.web_url)
+                
+                    let details = NewsSearchDetails(thumbnailURL: imagesDictionary["thumb"] ?? "", detailImageURL: imagesDictionary["watch"] ?? "", headline: ele.headline.main, abstract: ele.abstract, webURL: ele.web_url)
                     detailsArray.append(details)
                 }
                 completion(.success(detailsArray))
